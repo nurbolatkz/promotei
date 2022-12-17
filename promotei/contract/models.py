7139e8642b2f23f43f500920b286002869b1f398
nurbolatkz <nurbolat.duisenbek@narxoz.kz>
2022-12-17 20:55:54 +0600

    create model contract and document

--- promotei/contract/models.py
+++ promotei/contract/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from document.models import IdentityNumber
+
+
+# Create your models here.
+class StatusChoices(models.TextChoices):
+        SENDED = 'SENDED'
+        RECEIVED = 'RECEIVED'
+        ACCEPTED = "ACCEPTED"
+
+
+class Contract(models.Model):
+    renter = models.ForeignKey(IdentityNumber, on_delete=models.CASCADE, related_name='renter')
+    receiver = models.ForeignKey(IdentityNumber, on_delete=models.CASCADE, related_name='receiver')
+    status = models.CharField(max_length=10, choices=StatusChoices.choices, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.FileField()
+
--- promotei/document/models.py
+++ promotei/document/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class IdentityNumber(models.Model):
+    indentity_number = models.CharField(max_length=12, unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    birth_place = models.TextField()
+    
+
+class Esp(models.Model):
+    indentity_number = models.ForeignKey(IdentityNumber,on_delete=models.CASCADE)
+    esp = models.FileField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    expiration_date = models.DateTimeField()
