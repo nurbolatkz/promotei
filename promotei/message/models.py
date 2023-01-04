9759a162a75f28c0ab3937335de01072d1bbd9b6
nurbolatkz <nurbolat.duisenbek@narxoz.kz>
2023-01-04 15:58:20 +0600

    add message model

--- promotei/message/models.py
+++ promotei/message/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from user.models import CustomUser
+from contract.models import Contract
 
 # Create your models here.
+class Message(models.Model):
+    sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="sender")
+    reciever = models.ForeignKey(CustomUser,on_delete=models.CASCADE, related_name='receiver')
+    contract = models.ForeignKey(Contract, on_delete=models.CASCADE, related_name='contract')
+    msg_content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+    is_archived = models.BooleanField(default=False)
