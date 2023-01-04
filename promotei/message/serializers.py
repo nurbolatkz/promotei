1f551d689eeb8d2b8a6c41a8baae78dc85bf1b0c
nurbolatkz <nurbolat.duisenbek@narxoz.kz>
2023-01-04 16:09:53 +0600

    add serilizer for message model

--- promotei/message/serializers.py
+++ promotei/message/serializers.py
@@ -1,2 +1,16 @@
 from rest_framework import serializers
 from message.models import Message
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = [
+            'sender',
+            'receiver',
+            'contact',
+            'is_read',
+            'msg_content',
+            'created_at', 
+            'is_archived'
+        ]
