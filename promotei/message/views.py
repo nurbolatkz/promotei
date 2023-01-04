2471ad6416fda16b35f65baa6a2f59b7fa5592ae
nurbolatkz <nurbolat.duisenbek@narxoz.kz>
2023-01-04 16:48:40 +0600

    delete unused imports in message.views

--- promotei/message/views.py
+++ promotei/message/views.py
@@ -1,5 +1,5 @@
 # Create your views here.
-from rest_framework import generics, permissions, viewsets
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from message.models import Message
