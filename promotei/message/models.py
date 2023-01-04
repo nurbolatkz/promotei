201d595a9f8b3c745c3250fce7b4dfe02cf1147e
nurbolatkz <nurbolat.duisenbek@narxoz.kz>
2023-01-04 16:06:58 +0600

    add migrations for message model

--- promotei/message/models.py
+++ promotei/message/models.py
@@ -4,9 +4,9 @@ from contract.models import Contract
 
 # Create your models here.
 class Message(models.Model):
-    sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="sender")
-    reciever = models.ForeignKey(CustomUser,on_delete=models.CASCADE, related_name='receiver')
-    contract = models.ForeignKey(Contract, on_delete=models.CASCADE, related_name='contract')
+    sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="message_sender")
+    reciever = models.ForeignKey(CustomUser,on_delete=models.CASCADE, related_name='message_receiver')
+    contract = models.ForeignKey(Contract, on_delete=models.CASCADE, related_name='message_contract')
     msg_content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
