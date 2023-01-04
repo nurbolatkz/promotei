fe9292998d5ed24b6f4b8f2f138adde4e34d72f0
nurbolatkz <nurbolat.duisenbek@narxoz.kz>
2023-01-04 16:48:08 +0600

    delete unused imports in contract.views

--- promotei/contract/views.py
+++ promotei/contract/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from rest_framework import generics, permissions, viewsets
+from rest_framework import generics, viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from contract.models import Contract
