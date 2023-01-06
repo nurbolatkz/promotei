5109feb93edaa99a2fc0c55aa61120ef8862785c
nurbolatkz <nurbolat.duisenbek@narxoz.kz>
2023-01-06 12:13:19 +0600

    register admin site

--- promotei/contract/admin.py
+++ promotei/contract/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from contract.models import Contract
+from contract.models import Contract, ContractTemplate
 
 # Register your models here.
 admin.site.register(Contract)
+admin.site.register(ContractTemplate)
