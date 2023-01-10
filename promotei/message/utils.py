2e5501649f5ea64eb81f9158b3fe6e556e2323c1
nurbolatkz <nurbolat.duisenbek@narxoz.kz>
2023-01-10 15:37:02 +0600

    add accepted or declined in message.utils

--- promotei/message/utils.py
+++ promotei/message/utils.py
@@ -9,15 +9,19 @@ def create_or_update_message(sender, receiver, contract):
         return
     
     message_content = ''
+    
     is_create_new_message = False
+    is_accepted_or_declined = False
     if contract.status == 'CREATED':
         message_content = f'{sender.indentity_number.first_name} {sender.indentity_number.last_name}  создал договор'
         is_create_new_message = True
     elif contract.status == 'SENDED':
         message_content = f'{sender.indentity_number.first_name} {sender.indentity_number.last_name}  вам отправил(-а) договор'
     elif contract.status == 'ACCEPTED':
+        is_accepted_or_declined = True
         message_content = f'{sender.indentity_number.first_name} {sender.indentity_number.last_name}  вам принял(-а) договор'
     elif contract.status == 'DECLINED':
+        is_accepted_or_declined =  True
         message_content = f'{sender.indentity_number.first_name} {sender.indentity_number.last_name} +  вам отклонил(-а) договор'
     
     if is_create_new_message:
@@ -28,6 +32,10 @@ def create_or_update_message(sender, receiver, contract):
     else:
         try:
             message = Message.objects.get(contract=contract)
+            if is_accepted_or_declined:
+                message.is_archived = True
+                message.is_read =True
+            message.msg_content = message_content 
         except:
             return
     return message
